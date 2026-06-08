02932e7400316be26bbffaee4726b5e592caac94
Aureliano <aurelianodelvasto@gmail.com>
2026-06-08 10:33:34 +0000

    test start bat

--- start.bat
+++ start.bat
@@ -1,4 +1,17 @@
 @echo off
 cd /d "%~dp0"
-pythonw "%~dp0avvia.pyw" 2>nul
-if errorlevel 1 python "%~dp0avvia.pyw"
+
+if exist "%~dp0pythonw.exe" (
+set "TCL_LIBRARY=%~dp0tcl\tcl8.6"
+set "TK_LIBRARY=%~dp0tcl\tk8.6"
+"%~dp0pythonw.exe" "%~dp0avvia.pyw"
+goto :eof
+)
+
+where pythonw >nul 2>nul
+if not errorlevel 1 (
+pythonw "%~dp0avvia.pyw"
+goto :eof
+)
+
+python "%~dp0avvia.pyw"
