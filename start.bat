5a37bc841ccfc9836e1f7603bb2e2b054398c4e7
Aureliano <aurelianodelvasto@gmail.com>
2026-06-08 09:36:30 +0000

    prova

--- start.bat
+++ start.bat
@@ -1,5 +1,4 @@
 @echo off
 cd /d "%~dp0"
-set "TCL_LIBRARY=%~dp0tcl\tcl8.6"
-set "TK_LIBRARY=%~dp0tcl\tk8.6"
-"%~dp0pythonw.exe" "%~dp0avvia.pyw"
+pythonw "%~dp0avvia.pyw" 2>nul
+if errorlevel 1 python "%~dp0avvia.pyw"
